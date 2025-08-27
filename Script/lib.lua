c4967d9ce4a328a8f1557759395db08970ce4b87
小云 <id_09128616@foxmail.com>
2025-08-27 14:43:31 +0800

    Update lib.lua

--- Script/lib.lua
+++ Script/lib.lua
@@ -1,6 +1,7 @@
 return {
     ["猪仔"] = "https://github.com/XiaoYunUwU/XiaoYunUwU/raw/main/Script%2FA%E5%B0%8F%E7%8C%AA.lua",
     ["森林中的99天"] = "https://raw.githubusercontent.com/XiaoYunUwU/XiaoYunUwU/refs/heads/main/Script/%E7%94%9F%E5%AD%9899%E5%A4%A9",
+    ["被遗弃"] = "https://raw.githubusercontent.com/XiaoYunUwU/XiaoYunUwU/refs/heads/main/Script/%E8%A2%AB%E9%81%97%E5%BC%83",
     ["力量传奇"] = "https://github.com/XiaoYunUwU/XiaoYunUwU/raw/main/Script/%E5%8A%9B%E9%87%8F%E4%BC%A0%E5%A5%87",
     ["吃掉世界"] = "https://github.com/XiaoYunUwU/XiaoYunUwU/raw/main/Script%2FA%E5%90%83%E4%B8%96%E7%95%8C",
     ["Break in (Story)"] = "https://github.com/XiaoYunUwU/XiaoYunUwU/raw/main/Script%2FBreak%20in",
