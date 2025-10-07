94a1b3edb07cc70a0a429a60777245db47ded8d0
XiaoYunUwU <id_09128616@foxmail.com>
2025-10-08 06:04:10 +0800

    Update lib.lua

--- Script/lib.lua
+++ Script/lib.lua
@@ -1,13 +1,3 @@
 return {
-    ["猪仔"] = "https://github.com/XiaoYunUwU/XiaoYunUwU/raw/main/Script%2FA%E5%B0%8F%E7%8C%AA.lua",
-    ["森林中的99天"] = "https://raw.githubusercontent.com/XiaoYunUwU/XiaoYunUwU/refs/heads/main/Script/%E7%94%9F%E5%AD%9899%E5%A4%A9",
-    ["被遗弃"] = "https://raw.githubusercontent.com/XiaoYunUwU/XiaoYunUwU/refs/heads/main/Script/%E8%A2%AB%E9%81%97%E5%BC%83",
-    ["力量传奇"] = "https://github.com/XiaoYunUwU/XiaoYunUwU/raw/main/Script/%E5%8A%9B%E9%87%8F%E4%BC%A0%E5%A5%87",
-    ["吃掉世界"] = "https://github.com/XiaoYunUwU/XiaoYunUwU/raw/main/Script%2FA%E5%90%83%E4%B8%96%E7%95%8C",
-    ["Break in (Story)"] = "https://github.com/XiaoYunUwU/XiaoYunUwU/raw/main/Script%2FBreak%20in",
-    ["Field Trip Z"] = "https://github.com/XiaoYunUwU/XiaoYunUwU/raw/main/Script%2FField%20Trip%20Z",
-    ["极速传奇"] = "https://github.com/XiaoYunUwU/XiaoYunUwU/raw/main/Script%2F%E6%9E%81%E9%80%9F%E4%BC%A0%E5%A5%87",
-    ["虫脸"] = "https://raw.githubusercontent.com/XiaoYunUwU/XiaoYunUwU/refs/heads/main/Script/%E8%99%AB%E8%84%B8",
-    ["Fisch"] = "https://github.com/XiaoYunUwU/XiaoYunUwU/raw/main/Script%2FFisch",
-    ["忍者传奇"] = "https://github.com/XiaoYunUwU/XiaoYunUwU/raw/main/Script%2F%E5%BF%8D%E8%80%85%E4%BC%A0%E5%A5%87"
-}
+    ["无"] = "https://github.com/XiaoYunUwU/"
+} 
